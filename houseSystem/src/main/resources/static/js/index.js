a8a402871463b628f56b81ecb8f24d6194eae583
MKwindow <yuxunhe233@gmail.com>
2019-03-24 18:28:04 +0800

    更新网页
    修改了主页搜索栏
    增加了页面详细页

--- houseSystem/src/main/resources/static/js/index.js
+++ houseSystem/src/main/resources/static/js/index.js
@@ -30,3 +30,37 @@ layui.use(['laypage', 'layer'], function () {
         , hash: 'fenye' //自定义hash值
     });
 });
+
+//特效
+function searchToggle(obj, evt) {
+    var container = $(obj).closest('.search-wrapper');
+
+    if (!container.hasClass('active')) {
+        container.addClass('active');
+        evt.preventDefault();
+    }
+    else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
+        container.removeClass('active');
+        // clear input
+        container.find('.search-input').val('');
+        // clear and hide result container when we press close
+        container.find('.result-container').fadeOut(100, function () {
+            $(this).empty();
+        });
+    }
+}
+
+function submitFn(obj, evt) {
+    value = $(obj).find('.search-input').val().trim();
+
+    _html = "你输入的是: ";
+    if (!value.length) {
+        _html = "没有输入文本";
+    }
+    else {
+        _html += "<b>" + value + "</b>";
+    }
+    $(obj).find('.result-container').html('<span>' + _html + '</span>');
+    $(obj).find('.result-container').fadeIn(100);
+    evt.preventDefault();
+}
