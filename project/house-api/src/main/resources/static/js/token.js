095a81879ecfc2298479beddfa29621b1aab933a
sunxiaoyuan <2885253486@qq.com>
2019-03-29 16:49:49 +0800

    'delete'

--- project/house-api/src/main/resources/static/js/token.js
+++
@@ -1,8 +0,0 @@
-var token = localStorage.getItem("token");//获取名称为“key”的值
-if (token == null || token === "") {
-    var userform = document.getElementById("signform");
-    if (userform != null) {
-        popwindows();
-    }
-    window.location.href = houser_index_url;
-}
